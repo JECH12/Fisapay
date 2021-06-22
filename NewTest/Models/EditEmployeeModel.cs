4e1d77981defce80ed932dcd71b672d744b4c277
JECH12 <86272376+JECH12@users.noreply.github.com>
2021-06-21 20:05:35 -0500

    Add files via upload

---
+++ NewTest/Models/EditEmployeeModel.cs
@@ -0,0 +1,13 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewTest.Models
+{
+    public class EditEmployeeModel
+    {
+        public Employee Employee { get; set; }
+    }
+}
