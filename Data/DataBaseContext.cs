03d0f4f795a2fc6ee88019c6abbdb43650c5060e
JECH12 <86272376+JECH12@users.noreply.github.com>
2021-06-21 19:57:41 -0500

    Data

---
+++ Data/DataBaseContext.cs
@@ -0,0 +1,31 @@
+﻿using Domain;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace Data
+{
+    public class DatabaseContext : DbContext, IDisposable
+    {
+        public DatabaseContext()
+    : this("MainDatabase")
+        {
+            ((IObjectContextAdapter)this).ObjectContext.CommandTimeout = 300;
+        }
+
+        public DatabaseContext(string connectionString)
+            : base(connectionString)
+        {
+            ((IObjectContextAdapter)this).ObjectContext.CommandTimeout = 300;
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer<DatabaseContext>(null);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; }
+    }
+}
+
