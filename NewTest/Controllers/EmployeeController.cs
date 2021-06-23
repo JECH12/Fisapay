67b5732aef9404cbe92664c8feaa9f5bdf9c3309
JECH12 <86272376+JECH12@users.noreply.github.com>
2021-06-23 08:53:38 -0500

    Add files via upload

---
+++ NewTest/Controllers/EmployeeController.cs
@@ -0,0 +1,93 @@
+﻿using Domain;
+using NewTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NewTest.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        Business.Employee admin = new Business.Employee();
+
+        [Route("api/Employees")]
+        [HttpGet]
+        public IEnumerable<Employee> GetEmployees()
+        {
+            var employees = admin.GetEmployees();
+
+            return employees;
+        }
+
+        [Route("api/Employees/{searchTerm}")]
+        [HttpGet]
+        public IEnumerable<Employee> GetEmployees(string searchTerm)
+        {
+            var employees = admin.GetEmployees(searchTerm);
+
+            return employees;
+        }
+
+        [Route("api/AddEmployee")]
+        [HttpGet]
+        public EditEmployeeModel AddEmployee()
+        {
+            var model = new EditEmployeeModel();
+
+            return model;
+        }
+
+        [Route("api/AddEmployee")]
+        [HttpPost]
+        public IHttpActionResult AddEmployee(Employee employee)
+        {
+            admin.AddEmployee(employee);
+
+            return StatusCode(HttpStatusCode.OK);
+        }
+
+        [Route("api/DeleteEmployee/{employeeId}")]
+        [HttpPost]
+        public void DeleteCourse(Guid employeeId)
+        {
+            admin.DeleteEmployee(employeeId);
+        }
+
+        [Route("api/EditEmployee/{employeeId}")]
+        [HttpGet]
+        public EditEmployeeModel EditCourse(Guid employeeId)
+        {
+            var model = new EditEmployeeModel();
+            model.Employee = admin.GetEmployee(employeeId);
+            return model;
+        }
+
+        [Route("api/EditEmployee")]
+        [HttpPost]
+        public IHttpActionResult EditEmployee(Employee employee)
+        {
+            admin.EditEmployee(employee);
+            return StatusCode(HttpStatusCode.OK);
+        }
+
+        [Route("api/VerifyEmployee/{employeeDNI}")]
+        [HttpGet]
+        public bool VerifyEmployee(string employeeDNI)
+        {
+            var result = admin.VerifyEmployee(employeeDNI);
+            if (result)
+            {
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ NewTest/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+        public ActionResult Consult()
+        {
+            return View();
+        }
+
+        public ActionResult AddEmployee()
+        {
+            return View();
+        }
+
+        public ActionResult EditEmployee(Guid employeeId)
+        {
+            return View();
+        }
+    }
+}
